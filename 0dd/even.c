5c56d3bea5a2e55c1e011d1c12f9719e324b4447
vignash74 <30952587+vignash74@users.noreply.github.com>
2017-08-17 16:55:04 +0530

    odd/even
     give the output

---
+++ 0dd/even.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("Enter the number;");
+scanf("%d,&a");
+if(a%==o)
+printf("The number is even");
+else
+printf("The number is odd");
+getch()
+}
